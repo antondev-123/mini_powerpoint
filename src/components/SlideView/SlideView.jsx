47cb38bcd43952f2c875ec21807c24808a687733
dev0623 <sydoranton99@outlook.com>
2025-06-10 20:30:10 +0300

    Code highlighting fix

--- src/components/SlideView/SlideView.jsx
+++ src/components/SlideView/SlideView.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactMarkdown from "react-markdown";
+import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
@@ -7,22 +7,22 @@ import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
 const SlideView = ({ content, layout }) => (
   <div className={`slide ${layout}`}>
     <div className="slide-content">
-      <ReactMarkdown
+      <Markdown
         children={content}
         remarkPlugins={[remarkGfm]}
         components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\\w+)/.exec(className || "");
-            return !inline && match ? (
+          code({ node, className, children, ...rest }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
               <SyntaxHighlighter
                 children={String(children).replace(/\\n$/, "")}
                 style={okaidia}
                 language={match[1]}
                 PreTag="div"
-                {...props}
+                {...rest}
               />
             ) : (
-              <code className={className} {...props}>
+              <code className={className} {...rest}>
                 {children}
               </code>
             );
